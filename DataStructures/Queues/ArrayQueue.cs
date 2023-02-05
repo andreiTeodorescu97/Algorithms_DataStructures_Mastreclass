838dd1e3335e0efe67b218fe1593c4fa6e5cb22b
Teodorescu Andrei-Calin <andrei-calin.teodorescu@ing.com>
2023-02-05 15:38:58 +0200

    Added git ignore

---
+++ DataStructures/Queues/ArrayQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures.Queues
+{
+    public class ArrayQueue<T> : IEnumerable<T>
+    {
+        private T[] _queue;
+        private int _head;
+        private int _tail;
+
+        public int Count => _tail - _head;
+        public bool IsEmpty => Count == 0;
+
+        public ArrayQueue()
+        {
+            _queue = new T[4];
+        }
+
+        public ArrayQueue(int capacity)
+        {
+            _queue = new T[capacity];
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _head; i < _tail; i++)
+            {
+                yield return _queue[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enqueue(T item)
+        {
+            if (_queue.Length == _tail)
+            {
+                T[] lar = new T[Count * 2];
+                Array.Copy(_queue, lar, Count);
+                _queue = lar;
+            }
+
+            _queue[_tail++] = item;
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _queue[_head++] = default(T);
+
+            if (IsEmpty)
+            {
+                _head = _tail = 0;
+            }
+        }
+
+        public T Peek()
+        {
+            if(IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _queue[_head];
+        }
+    }
+}
