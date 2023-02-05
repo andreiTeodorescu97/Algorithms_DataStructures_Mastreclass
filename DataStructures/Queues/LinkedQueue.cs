85e4332f48511e104724699beac3e4ba84ef7c1a
Teodorescu Andrei-Calin <andrei-calin.teodorescu@ing.com>
2023-02-05 16:23:41 +0200

    Added Queue implementation

--- DataStructures/Queues/LinkedQueue.cs
+++ DataStructures/Queues/LinkedQueue.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using DataStructures.Lists.Singly;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DataStructures.Queues
 {
-    internal class LinkedQueue
+    public class LinkedQueue<T> : IEnumerable<T>
     {
+        private readonly SinglyLinkedList<T> _list = new SinglyLinkedList<T>();
+        public int Count => _list.Count;
+        public bool IsEmpty => _list.Count == 0;
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item); 
+        }
+
+        public void Dequeue()
+        {
+            _list.RemoveFirst();
+        }
+
+        public T Peek()
+        {
+            return _list.Head.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
     }
 }
---
+++ Tests/LinkedQueueTests.cs
@@ -0,0 +1,55 @@
+﻿using DataStructures.Queues;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LinkedQueueTests
+    {
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            var stack = new LinkedQueue<int>();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Count_EnqueueOneItem_ReturnsOne()
+        {
+            var stack = new LinkedQueue<int>();
+            stack.Enqueue(1);
+
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [Test]
+        public void DEnqueue_EmptyStack_ThrowsException()
+        {
+            var stack = new LinkedQueue<int>();
+            Assert.Throws<InvalidOperationException>(() => { stack.Dequeue(); });
+        }
+
+        [Test]
+        public void Peek_EnqueueTwoItems_ReturnsHeadItem()
+        {
+            var stack = new LinkedQueue<int>();
+            stack.Enqueue(1);
+            stack.Enqueue(2);
+
+            Assert.AreEqual(1, stack.Peek());
+        }
+
+        [Test]
+        public void Peek_EnqueueTwoItemsDEnqueue_ReturnsHeadItem()
+        {
+            var stack = new LinkedQueue<int>();
+            stack.Enqueue(1);
+            stack.Enqueue(2);
+            stack.Dequeue();
+
+            Assert.AreEqual(2, stack.Peek());
+        }
+    }
+}
